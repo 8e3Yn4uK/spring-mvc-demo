41562a4591e74b69fcb193e6c11b449c39cb21b6
Oleksandr <sashko.kondratenko@gmail.com>
2019-02-12 02:08:30 +0100

    MVC Form Tags

--- src/com/springdemo/mvc/Student.java
+++ src/com/springdemo/mvc/Student.java
@@ -1,12 +1,14 @@
 package com.springdemo.mvc;
 
-import java.util.LinkedHashMap;
+        import java.util.LinkedHashMap;
 
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
+    private String favoriteLanguage;
+    private String[] operatingSystems;
 
 
     public Student() {
@@ -37,4 +39,19 @@ public class Student {
         this.lastName = lastName;
     }
 
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public String[] getOperatingSystems() {
+        return operatingSystems;
+    }
+
+    public void setOperatingSystems(String[] operatingSistems) {
+        this.operatingSystems = operatingSistems;
+    }
 }
--- web/WEB-INF/view/student-confirmation.jsp
+++ web/WEB-INF/view/student-confirmation.jsp
@@ -1,3 +1,6 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+
 <head>
     <title>Student Confirmation</title>
 </head>
@@ -9,6 +12,21 @@ The student is confirmed:  ${student.firstName} ${student.lastName}
 <br><br>
 
 Country: ${student.country}
+
+<br><br>
+
+Favorite Language: ${student.favoriteLanguage}
+
+<br><br>
+
+Operating Systems:
+
+<ul>
+    <c:forEach var="temp" items="${student.operatingSystems}">
+    <li> ${temp} </li>
+    </c:forEach>
+</ul>
+
 </body>
 
 </html>
